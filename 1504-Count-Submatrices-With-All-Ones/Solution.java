7be11e05556f0ded1b1f438341140c3407370c3e
Harshit9369 <harshitagrwl911@gmail.com>
2025-08-21 14:55:14 +0530

    Add: 1504. Count Submatrices With All Ones

---
+++ 1504-Count-Submatrices-With-All-Ones/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    // Paste your final solution here.
+    // You can write a main() to test locally; LeetCode ignores it.
+    public static void main(String[] args) {
+        // quick local check
+        System.out.println("Ready to code: Solution");
+    }
+}
